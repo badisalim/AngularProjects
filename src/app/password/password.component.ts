4826591aaef87c6d00d28386c0b51eaf2040c459
badisalim <badisalim@gmail.com>
2019-06-21 09:16:30 +0200

    uptodate

--- src/app/password/password.component.ts
+++ src/app/password/password.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { isNumber } from 'util';
+import { FormBuilder, Form, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-password',
@@ -7,14 +8,20 @@ import { isNumber } from 'util';
   styleUrls: ['./password.component.css']
 })
 export class PasswordComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  edited = false;
+  passwordMessage = '';
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      password1: [''],
+      password2: []
+    });
   }
-  checkPassword = (password1, password2) => {
+  checkPassword(password1, password2) {
     if (password1 === '' && password2 === '') {
-      console.log('Please enter Password')
+      console.log('Please enter Password');
       return false;
     } else if (password1 !== password2) {
       return false;
@@ -22,24 +29,35 @@ export class PasswordComponent implements OnInit {
     return true;
   }
 
-  isLargerThan8 = password => {
+  isLargerThan8(password) {
     return password.length > 8;
   }
 
-  isNumber = password => {
+  isNumber(password) {
     const regExp = /[0-9]/;
     return regExp.test(password);
   }
 
-  isLowerCase = password => {
+  isLowerCase(password) {
     const regExp = /[a-z]/;
     return regExp.test(password);
   }
-  isUpperCase = password => {
+  isUpperCase(password) {
     const regExp = /[A-Z]/;
     return regExp.test(password);
   }
-  verification = (password1, password2) => {
-    return this.checkPassword(password1, password2) && this.isLargerThan8(password1) && isNumber(password1) && this.isLowerCase(password1) && this.isUpperCase(password1) ? true : false;
+  submit() {
+    const { password1, password2 } = this.form.value;
+    if (
+      this.checkPassword(password1, password2) &&
+      this.isLargerThan8(password1) &&
+      isNumber(password1) &&
+      this.isLowerCase(password1) &&
+      this.isUpperCase(password1)
+    ) {
+      this.passwordMessage = 'Password OK';
+    } else {
+      this.passwordMessage = 'Password not OK';
+    }
   }
 }
